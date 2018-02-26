d3ab51c049d70aec93c04b9331446f03bcde0967
billy1816 <billy1816_@hotmail.com>
2018-02-26 18:03:01 +0100

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -17,10 +17,10 @@ window.onload = function(){
    inicializar();
    if (comprobar()){
     corregirNumber();
-    corregirNombre();
     corregirSelect();
     corregirCheckbox();
     corregirCheckbox1();
+    corregirNombre();
     presentarNota();
    }
    return false;
@@ -281,8 +281,8 @@ function comprobar(){
     document.getElementsByTagName("h3")[3].focus();
     alert("Selecciona una opció del checkbox(2)");
     return false;
-    } if (f.elements[4].value=="") {
-    f.elements[4].focus();
+    } if (f.elements[2].value=="") {
+    f.elements[2].focus();
     alert("Escriu un nom");
     return false;
     } else  return true;
