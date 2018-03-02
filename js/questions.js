b9be47c5dad057ea8dae06a6369378db61cf4104
billy1816 <billy1816_@hotmail.com>
2018-03-02 18:46:17 +0100

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -91,7 +91,7 @@ function gestionarXml(dadesXml){
   //Nom
  var tituloInput1=xmlDoc.getElementsByTagName("title")[4].innerHTML;
  ponerDatosInputHtml1(tituloInput1);
- nombreSecreto=xmlDoc.getElementsByTagName("answer")[7].innerHTML;
+ nombreSecreto=xmlDoc.getElementsByTagName("answer")[6].innerHTML;
 
  //CHECKBOX2
  //Recuperamos el título y las opciones, guardamos las respuestas correctas
