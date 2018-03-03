bbc938b0dc3c512d2c7ef92481a3bbbcf8ad9166
billy1816 <billy1816_@hotmail.com>
2018-03-03 12:44:43 +0100

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -15,6 +15,7 @@ var nota = 0;  //nota de la prueba sobre 3 puntos (hay 3 preguntas)
 //Después de cargar la página (onload) se definen los eventos sobre los elementos entre otras acciones.
 window.onload = function(){ 
 
+ document.getelementBy
  //CORREGIR al apretar el botón
  formElement=document.getElementById('myform');
  formElement.onsubmit=function(){
